c0391c57961a6b52f8d1044aa3092a7dfab2ed19
ARNABTINT <91847017+ARNABTINT@users.noreply.github.com>
2023-09-30 18:46:26 +0530

    Revert "Update BinarySearch.java"

--- java/BinarySearch.java
+++ java/BinarySearch.java
@@ -10,17 +10,13 @@ public class BinarySearch {
     static int binarySearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
-        while (start <= end)
-        {
+        while (start <= end) {
             int mid = end + ((start - end) / 2);
-            if (arr[mid] < target)
-            {
+            if (arr[mid] < target) {
                 start = mid + 1;
-            } else if (arr[mid] > target)
-            {
+            } else if (arr[mid] > target) {
                 end = mid - 1;
-            } else
-            {
+            } else {
                 return mid;
             }
         }
